bd0177231d8c58f751af3a6599d8ac0bc26b444e
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2020-08-22 16:17:34 -0300

    reveiving props in EditableTimerList

--- src/index.js
+++ src/index.js
@@ -6,11 +6,32 @@ import helpers from './helpers.js';
 
 
 class TimerDashboard extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Practice squat',
+        project: 'Gym Chores',
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      }
+    ]
+  }
+
   render() {
     return (
       <div className='ui three column centered grid'>
         <div className='column'>
-          <EditableTimerList />
+          <EditableTimerList 
+            timers={this.state.timers}
+          />
           <ToggleableTimerForm
             isOpen={true}
           />
@@ -42,22 +63,19 @@ class ToggleableTimerForm extends React.Component {
 
 class EditableTimerList extends React.Component {
   render() {
+    const timers = this.props.timers.map( (timer) => {
+      <EditableTimer
+        key={timer.id}
+        id={timer.id}
+        title={timer.title}
+        project={timer.project}
+        elapsed={timer.elapsed}
+        runningSince={timer.runningSince}
+      />
+    })
     return (
       <div id='timers'>
-        <EditableTimer 
-          title='Learn React'
-          project='Web Domination'
-          elapsed='8986300'
-          runningSince={null}
-          editFormOpen={false}
-        />
-        <EditableTimer 
-          title='Learn extreme ironing'
-          project='World Domination'
-          elapsed='3890985'
-          runningSince={null}
-          editFormOpen={false}
-        />
+        {timers}
       </div>
     )
   }
